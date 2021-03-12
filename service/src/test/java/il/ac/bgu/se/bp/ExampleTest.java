645ecaba3a48a2458f36463cb3999632595730ae
alexabramov2610 <alexabramov2610@gmail.com>
2021-03-12 12:27:28 +0200

    ci script - check

---
+++ service/src/test/java/il/ac/bgu/se/bp/ExampleTest.java
@@ -0,0 +1,14 @@
+package il.ac.bgu.se.bp;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void assertJunk() {
+        assertTrue(true);
+    }
+}
