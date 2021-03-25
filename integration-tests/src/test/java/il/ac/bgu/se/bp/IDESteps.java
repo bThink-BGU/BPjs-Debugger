09d365b169dc5a5076ebd89778e0eea58445d5a3
Alex Abramov <alexander.abramov@nice.com>
2021-03-26 00:21:26 +0200

    integration tests

--- integration-tests/src/test/java/il/ac/bgu/se/bp/IDESteps.java
+++ integration-tests/src/test/java/il/ac/bgu/se/bp/IDESteps.java
@@ -7,36 +7,29 @@ import il.ac.bgu.se.bp.rest.request.DebugRequest;
 import il.ac.bgu.se.bp.rest.response.BooleanResponse;
 import il.ac.bgu.se.bp.rest.socket.StompPrincipal;
 import il.ac.bgu.se.bp.socket.state.BPDebuggerState;
-import il.ac.bgu.se.bp.utils.Pair;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.util.StringUtils;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 
 import static il.ac.bgu.se.bp.code.CodeFilesHelper.getCodeByFileName;
+import static il.ac.bgu.se.bp.common.Utils.*;
 import static org.junit.Assert.*;
 
 @ContextConfiguration(classes = IDECommonTestConfiguration.class)
 public class IDESteps {
 
     private String userId;
-
-    @Autowired
-    TestService testService;
-
-    @Autowired
-    MockSessionHandler sessionHandler;
-
     private BooleanResponse lastResponse;
+
+    @Autowired
+    private TestService testService;
+
+    @Autowired
+    private MockSessionHandler sessionHandler;
 
     @Given("I have connected to websocket with (.*) and (.*)")
     public void iHaveConnectedToWebsocketWithSessionIdAndUserId(String sessionId, String userId) {
@@ -61,27 +54,6 @@ public class IDESteps {
         waitUntilPredicateSatisfied(() -> sessionHandler.getLastDebuggerStates() != null, 1000, 3);
     }
 
-    private void waitUntilPredicateSatisfied(Callable<Boolean> predicate, int timeToSleep, int maxToTry) {
-        try {
-            for (int i = 0; i < maxToTry; i++) {
-                if (predicate.call()) {
-                    return;
-                }
-                sleep(timeToSleep);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void sleep(int timeToSleep) {
-        try {
-            Thread.sleep(timeToSleep);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Then("The response should be (.*) with errorCode (.*)")
     public void theResponseShouldBe(String result, String errorCode) {
         assertEquals(strToBoolean(result), lastResponse.isSuccess());
@@ -102,56 +74,5 @@ public class IDESteps {
 
         strToStringVarsList(stringVars).forEach(var -> assertEquals(var.getRight(), env.get(var.getLeft())));
         strToDoubleVarsList(doubleVars).forEach(var -> assertEquals(var.getRight(), strToDouble(env.get(var.getLeft())), 0));
-    }
-
-
-    private boolean isNull(String str) {
-        return str.equals("null");
-    }
-
-    private List<Pair<String, String>> strToStringVarsList(String stringVars) {
-        return StringUtils.isEmpty(stringVars) ? new LinkedList<>() :
-                Arrays.stream(stringVars.split(",")).map(strVar -> Pair.of(getVar(strVar), getStrVal(strVar))).collect(Collectors.toList());
-    }
-
-    private List<Pair<String, Double>> strToDoubleVarsList(String doubleVars) {
-        return StringUtils.isEmpty(doubleVars) ? new LinkedList<>() :
-                Arrays.stream(doubleVars.split(",")).map(strVar -> Pair.of(getVar(strVar), getDoubleVal(strVar))).collect(Collectors.toList());
-    }
-
-    private String getVar(String str) {
-        return splitAndGetBy(str, "=", 0);
-    }
-
-    private String getStrVal(String str) {
-        return splitAndGetBy(str, "=", 1);
-    }
-
-    private Double getDoubleVal(String str) {
-        return strToDouble(splitAndGetBy(str, "=", 1));
-    }
-
-    private String splitAndGetBy(String str, String regex, int i) {
-        return str.split(regex)[i];
-    }
-
-    private boolean strToBoolean(String str) {
-        return Boolean.parseBoolean(str);
-    }
-
-    private Double strToDouble(String str) {
-        return Double.parseDouble(str);
-    }
-
-    private List<String> strToStringList(String strings) {
-        return Arrays.stream(strings.split(",")).collect(Collectors.toList());
-    }
-
-    private List<Double> strToDoubleList(String doubles) {
-        return Arrays.stream(doubles.split(",")).map(Double::parseDouble).collect(Collectors.toList());
-    }
-
-    private List<Integer> strToIntList(String breakpoints) {
-        return Arrays.stream(breakpoints.split(",")).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
