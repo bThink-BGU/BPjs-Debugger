351d6c6831451a5d77d5808ddd0d2bb797d5c18b
Alex Abramov <alexander.abramov@nice.com>
2021-04-03 14:30:47 +0300

    CORS fix

--- controller/src/main/java/il/ac/bgu/se/bp/app/Application.java
+++ controller/src/main/java/il/ac/bgu/se/bp/app/Application.java
@@ -3,7 +3,10 @@ package il.ac.bgu.se.bp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages="il.ac.bgu.se.bp")
@@ -12,4 +15,15 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
 }
