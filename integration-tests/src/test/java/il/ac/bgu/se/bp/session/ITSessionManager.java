19f80077932fe8d41df38802783bc6e950b8f6e5
Alex Abramov <alexander.abramov@nice.com>
2021-05-18 19:59:38 +0300

    code cleanup

---
+++ integration-tests/src/test/java/il/ac/bgu/se/bp/session/ITSessionManager.java
@@ -0,0 +1,22 @@
+package il.ac.bgu.se.bp.session;
+
+import il.ac.bgu.se.bp.socket.console.ConsoleMessage;
+import il.ac.bgu.se.bp.socket.state.BPDebuggerState;
+import il.ac.bgu.se.bp.socket.status.Status;
+
+public interface ITSessionManager {
+
+    void cleanUserMockData(String userId);
+    boolean isUserFinishedRunning(String userId);
+
+    void addUserStatus(String userId, Status userStatus);
+    Status getUsersStatus(String userId);
+    void removeUsersStatus(String userId);
+
+    void addDebuggerState(String userId, BPDebuggerState debuggerState);
+    BPDebuggerState getUsersLastDebuggerState(String userId);
+
+    void addConsoleMessage(String userId, ConsoleMessage consoleMessage);
+
+
+}
